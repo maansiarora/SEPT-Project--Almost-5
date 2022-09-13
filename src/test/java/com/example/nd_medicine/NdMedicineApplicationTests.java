abb4178ed8d709e757e0ce875bec3d7c81fa809f
ms-hacker404 <jtacd18@gmail.com>
2022-09-14 05:03:45 +1000

    File Initial

---
+++ src/test/java/com/example/nd_medicine/NdMedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nd_medicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdMedicineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
