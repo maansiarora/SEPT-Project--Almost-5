1e52c1056562e69673f9b755a2ab2570b7b39440
suhavnimathur <s3874834@student.rmit.edu.au>
2022-09-14 02:47:49 +1000

    Doctor SignUp

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'pages/doctorSignUp.dart';
 import 'pages/choiceLogin.dart';
 
 void main() => runApp(MaterialApp(initialRoute: '/choiceLogin', routes: {
       '/choiceLogin': (context) => const choiceLogin(),
+      '/doctorSignUp': (context) => const doctorSignUp(),
     }));
