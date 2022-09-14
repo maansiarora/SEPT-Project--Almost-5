a6d53ec6ea0b89a64b0f5d7eecff3be53a218f9b
Darian <s3899332@student.rmit.edu.au>
2022-09-14 21:29:00 +1000

    Finished Availability and Registration Controllers. Created DoctorAvailability and DoctorAvailabilityRepository objects. Functional.

---
+++ src/main/java/com/example/nd_medicine/service/DoctorService.java
@@ -0,0 +1,21 @@
+package com.example.nd_medicine.service;
+
+import com.example.nd_medicine.entity.Doctor;
+import com.example.nd_medicine.entity.Patient;
+import com.example.nd_medicine.repository.DoctorRepository;
+import com.example.nd_medicine.repository.PatientRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class DoctorService {
+
+
+}
