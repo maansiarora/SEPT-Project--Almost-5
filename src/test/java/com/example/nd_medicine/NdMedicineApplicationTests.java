ae3f76fd02384b600a7021b3c749d3677d2e04b8
Darian <s3899332@student.rmit.edu.au>
2022-09-16 02:46:49 +1000

    Start of Junit for Appointment

--- src/test/java/com/example/nd_medicine/NdMedicineApplicationTests.java
+++ src/test/java/com/example/nd_medicine/NdMedicineApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.nd_medicine;
 
+import com.example.nd_medicine.entity.Appointment;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NdMedicineApplicationTests {
 
+    @BeforeAll
+    public void setup() {
+
+        Appointment testAppointment = new Appointment();
+
+
+    }
+
     @Test
-    void contextLoads() {
+    public void test2() {
+
+        // Test 2
+
     }
 
 }
