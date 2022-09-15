e9de2390f0595795ac384bf0113eb92ab8b4107b
ms-hacker404 <jtacd18@gmail.com>
2022-09-15 17:00:35 +1000

    Appointment Entity and service

---
+++ src/main/java/com/example/nd_medicine/repository/AppointmentRepository.java
@@ -0,0 +1,19 @@
+package com.example.nd_medicine.repository;
+
+
+import com.example.nd_medicine.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> findAppointmentsByDoctorId(Long doctorId);
+    List<Appointment> findAppointmentsByPatientId(Long patientId);
+    Appointment findAppointmentByAppointmentId(Long appointmentId);
+
+
+
+}
