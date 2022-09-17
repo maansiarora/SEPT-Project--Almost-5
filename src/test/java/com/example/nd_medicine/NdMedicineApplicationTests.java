8be92904c4b0e769b128677607124c9a94a5069a
Darian <s3899332@student.rmit.edu.au>
2022-09-17 23:37:19 +1000

    Created First MVC Test (Issue with JSON Path) (Pushed for Consultation)

--- src/test/java/com/example/nd_medicine/NdMedicineApplicationTests.java
+++ src/test/java/com/example/nd_medicine/NdMedicineApplicationTests.java
@@ -1,67 +1,61 @@
 package com.example.nd_medicine;
 
+import com.example.nd_medicine.controller.AppointmentController;
 import com.example.nd_medicine.entity.Appointment;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.nd_medicine.service.AppointmentService;
+import com.jayway.jsonpath.spi.json.GsonJsonProvider;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.http.MediaType;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = AppointmentController.class)
 class NdMedicineApplicationTests {
 
-    boolean result;
+    @Autowired
+    private MockMvc mvc;
 
-    // Create Appointment Object for Testing
-    Appointment testAppointment = new Appointment();
-    Date appointmentStart = new Date();
-    Date appointmentEnd = new Date();
-    Calendar myCalendar = new GregorianCalendar();
+    @MockBean
+    private AppointmentService service;
 
-    @BeforeEach
-    public void setup() {
-
-        // Reset Testing Objects
-        testAppointment.resetObject();
-        myCalendar.clear();
-
+    @Before
+    public void setUp() throws Exception {
     }
 
     @Test
-    public void Test1() {
-
-        // Hardcode Testing Object
-        testAppointment.setId(100L);
-        testAppointment.setDoctorId(1001L);
-        testAppointment.setPatientId(2001L);
-
-        // Set Date Objects
-        myCalendar.set(2022, 2, 11, 12, 30, 00);
-        testAppointment.setStartTime(myCalendar.getTime());
-        myCalendar.set(2022, 2, 11, 13, 00, 00);
-        testAppointment.setEndTime(myCalendar.getTime());
+    public void given_when_thenReturnString() throws Exception {
 
         // Assert that Retrieving Output of testAppointment Returns Correct Output
+        // Leftover Code (Ignore)
         String expectedOut = "Appointment{appointmentId=100, patientId=2001, doctorId=1001, appointmentStartTime='Fri Mar 11 12:30:00 AEDT 2022', appointmentEndTime='Fri Mar 11 13:00:00 AEDT 2022'}";
-        assertEquals(expectedOut, testAppointment.toString());
 
+        // MVC
+        given(service.toString()).willReturn(expectedOut);
+
+        mvc.perform(post("/appointment/createAppointment").contentType(MediaType.APPLICATION_JSON).content("{\"patientId\" : 2001,\"appointmentId\" : 100, \"doctorId\" : 1001}")).andExpect(jsonPath("$.appointmentId", is(100)));
+        verify(service, VerificationModeFactory.times(1));
+        reset(service);
+
+        // Leftover Code (Ignore)
         System.out.println("Expected Out: " + expectedOut);
         System.out.println("Actual Out: " + expectedOut);
-
-    }
-
-    @Test
-    public void test2() {
-
-        // Test 2
-
 
     }
 
