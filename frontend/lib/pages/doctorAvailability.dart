453c67038afc8f9f1d3da222630f7a092017f11c
maansiarora <s3885529@student.rmit.edu.au>
2022-09-17 16:14:21 +1000

    doctor availability page - date picker color

--- frontend/lib/pages/doctorAvailability.dart
+++ frontend/lib/pages/doctorAvailability.dart
@@ -42,6 +42,23 @@ class _TextScreenState extends State<TextScreen> {
       initialDate: selectedDate,
       firstDate: DateTime(2000),
       lastDate: DateTime(2025),
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: const ColorScheme.light(
+              primary: Colors.deepPurpleAccent,
+              onPrimary: Colors.white,
+              onSurface: Colors.black,
+            ),
+            textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+                primary: Colors.deepPurpleAccent, // button text color
+              ),
+            ),
+          ),
+          child: child!,
+        );
+      },
     );
     if (selected != null && selected != selectedDate) {
       setState(() {
@@ -56,6 +73,23 @@ class _TextScreenState extends State<TextScreen> {
     final selected = await showTimePicker(
       context: context,
       initialTime: selectedTime,
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: const ColorScheme.light(
+              primary: Colors.deepPurpleAccent,
+              onPrimary: Colors.white,
+              onSurface: Colors.black,
+            ),
+            textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+                primary: Colors.deepPurpleAccent, // button text color
+              ),
+            ),
+          ),
+          child: child!,
+        );
+      },
     );
     if (selected != null && selected != selectedTime) {
       setState(() {
@@ -70,6 +104,23 @@ class _TextScreenState extends State<TextScreen> {
     final selected = await showTimePicker(
       context: context,
       initialTime: selectedTime2,
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: const ColorScheme.light(
+              primary: Colors.deepPurpleAccent,
+              onPrimary: Colors.white,
+              onSurface: Colors.black,
+            ),
+            textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+                primary: Colors.deepPurpleAccent, // button text color
+              ),
+            ),
+          ),
+          child: child!,
+        );
+      },
     );
     if (selected != null && selected != selectedTime2) {
       setState(() {
