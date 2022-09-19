0934c1b20ae3dfd53542961ec2c8f01f1833a2dc
ms-hacker404 <jtacd18@gmail.com>
2022-09-19 16:02:53 +1000

    chat controller config files errors in controller changing the database

--- src/main/java/com/example/nd_medicine/repository/ChatMessageRepository.java
+++ src/main/java/com/example/nd_medicine/repository/ChatMessageRepository.java
@@ -1,4 +1,16 @@
 package com.example.nd_medicine.repository;
 
-public class ChatMessageRepository {
+import com.amr.chatservice.model.ChatMessage;
+import com.amr.chatservice.model.MessageStatus;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository
+        extends MongoRepository<ChatMessage, String> {
+
+    long countBySenderIdAndRecipientIdAndStatus(
+            String senderId, String recipientId, MessageStatus status);
+
+    List<ChatMessage> findByChatId(String chatId);
 }
